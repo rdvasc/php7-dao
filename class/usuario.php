de472b45ac70b5d4c4f4dcc53b710bf913e5d7a9
rdvasc <rdvascon@gmail.com>
2020-04-19 13:21:55 -0300

    inclusao metodo update

--- class/usuario.php
+++ class/usuario.php
@@ -40,49 +40,9 @@ public function setDtcadastro($value) {
 	$this->dtcadastro=$value;
 }
 
-public function loadById($id) {
-	$sql = new Sql();
-	$results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(":ID"=>$id));
-
-		if (count($results) > 0 ) {
-			$row= $results[0];
-
-			$this->setIdusuario($row['idusuario']);
-			$this->setDeslogin($row['deslogin']);
-			$this->setDessenha($row['dessenha']);
-			$this->setDtcadastro(new DateTime($row['dtcadastro']));
-			
-		}
-}
-
-public  static function getList() {
-	$sql = new sql();
-	return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
-}
-
-public static function search($login) {
-	$sql = new Sql();
-	//return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE '%". $login ."%'");
-	return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin ",array(":SEARCH"=>"%".$login."%"));
-
-}
-
-public function login($login, $password) {
-	$sql = new Sql();
-	$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin=:LOGIN AND dessenha=:PASSWORD", array(
-		":LOGIN"=>$login,
-		":PASSWORD"=>$password));
-
-		if (count($results) > 0 ) {
-			$row= $results[0];
-
-			$this->setIdusuario($row['idusuario']);
-			$this->setDeslogin($row['deslogin']);
-			$this->setDessenha($row['dessenha']);
-			$this->setDtcadastro(new DateTime($row['dtcadastro']));		
-		} else {
-			throw new Exception("Login e/ou senha inválidos.");
-		}
+public function __construct($login="", $password="") {
+	$this->setDeslogin($login);
+	$this->setDessenha($password);
 }
 
 public function __toString() {
@@ -93,6 +53,82 @@ public function __toString() {
 		"dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
 	));
 }
+
+public function setData($data) {
+	$this->setIdusuario($data['idusuario']);
+	$this->setDeslogin($data['deslogin']);
+	$this->setDessenha($data['dessenha']);
+	$this->setDtcadastro(new DateTime($data['dtcadastro']));	
+}
+
+public function login($login, $password) {
+	$sql = new Sql();
+	$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin=:LOGIN AND dessenha=:PASSWORD", array(
+		":LOGIN"=>$login,
+		":PASSWORD"=>$password));
+
+		if (count($results) > 0 ) {
+			$this->setData($result[0]);
+
+		} else {
+			throw new Exception("Login e/ou senha inválidos.");
+		}
+}
+
+public function loadById($id) {
+	$sql = new Sql();
+	$results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(":ID"=>$id));
+
+		if (count($results) > 0 ) {
+			$this->setData($results[0]);
+			
+		}
+}
+
+public static function search($login) {
+	$sql = new Sql();
+	//return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE '%". $login ."%'");
+	return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin ",array(":SEARCH"=>"%".$login."%"));
+
+}
+
+public  static function getList() {
+	$sql = new sql();
+	return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+}
+
+
+public function insert() {
+	$sql = new SQL();
+	$results = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD)", array(
+		':LOGIN'=>$this->getDeslogin(),
+		':PASSWORD'=>$this->getDessenha()
+	));
+	
+	if (count($results) > 0) {
+		$this->setData($results[0]);
+	}
+}
+
+public function update($login, $password) {
+	$this->setDeslogin($login);
+	$this->setDessenha($password);
+
+	$sql = new Sql();
+	$sql->query("UPDATE tb_usuarios SET deslogin= :LOGIN, desenha= :PASSWORD WHERE idusuario = :ID", array (
+		':LOGIN'=>$this->getDeslogin(),
+		':PASSWORD'=>$this->getDessenha(),
+		':ID'=>$this->getIdusuario()
+	));
+
+
+
+
+}
+
+
+
+
 
 }
 
--- index.php
+++ index.php
@@ -28,12 +28,33 @@ $lista = Usuario::search("se");
 echo json_encode($lista);
 */
 
-
+/*
 $usuario = new Usuario();
 $usuario->login("user","12345");
 echo $usuario;
+*/
+/*
+$aluno = new Usuario();
+$aluno->setDeslogin("aluno");
+$aluno->setDessenha("54321");
+$aluno->insert();
+echo $aluno;
+*/
 
+/*
+//insersão de aluno
+$aluno = new Usuario("Roberto","582410");
+$aluno->insert();
+echo $aluno;
+*/
 
+$usuario = new Usuario();
+$usuario->loadById(5);
+echo $usuario;
+
+echo "<br/>";
+$usuario->update("Roberto","241058");
+echo $usuario;
 
 
  ?>
